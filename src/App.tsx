451d4f9d0c73ebedc57be45e8589da33cb8bd836
Sumanta <sumanta@xynocast.com>
2024-09-12 06:03:46 +0530

    first

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+// src/App.tsx
+import React from 'react';
+import Quiz from './Quiz';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <Quiz />
+    </div>
+  );
+};
+
+export default App;
